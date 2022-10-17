2f77332daf102f891c8db0347b138d29f4fa001e
RodriBau <rodri.bp.gg@gmail.com>
2022-10-17 13:15:33 -0400

    Empresa-Producto Editar-Nuevo

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import * as Yup from 'yup';
 import {useQuery, useMutation,gql} from '@apollo/client';
 import Link from "next/link";
 import Producto from '../componentes/cardProducto';
+import Empresa from '../componentes/cardEmpresa';
 
 const OBTENER_PRODUCTOS = gql`
 query ObtenerProductos {
@@ -34,6 +35,45 @@ export default function Index() {
               <Producto
                 Key={producto.id}
                 producto={producto}
+              />
+            ))}
+
+    </div>
+
+      </Layout>
+    </div>
+  )
+}
+
+//para empresas
+const OBTENER_EMPRESAS = gql`
+query ObtenerEmpresas {
+  obtenerEmpresas {
+    id
+    nombre_empresa
+    numero_sucursal
+    descripcion_empresa
+    telefono
+  }
+}`
+
+export default function Index() {
+
+  //obtener empresas de graphql
+  const{data,loading,error}=useQuery(OBTENER_EMPRESAS);
+  if (loading) {
+    return 'cargando....'
+  }
+
+  return (
+    <div>
+      <Layout>
+        <h1 className='text-2xl text-gray-800 font-light mb-4 '>Empresas</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-x-6 gap-y-10">
+            {data.obtenerEmpresas.map(empresa=>(
+              <Empresa
+                Key={empresa.id}
+                producto={empresa}
               />
             ))}
 
