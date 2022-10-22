b4e2e8d20afd35ac90f0fa4af3a9c8e900adbc76
fabian <fabianalmendro456@gmail.com>
2022-10-21 22:13:31 -0400

    correccion de dolar a bolivianos en cartas de productos y correccion de mensaje de error en tipo de producto

--- pages/nuevoproducto.js
+++ pages/nuevoproducto.js
@@ -160,7 +160,7 @@ const NuevoProducto = () => {
             empresa: Yup.string()
                     .required('La empresa es obligatoria'),
             tipo_producto: Yup.string()
-                            .required('El tipo es obligaorio')
+                            .required('Se debe seleccionar el tipo de producto')
         }),
         onSubmit: async valores => {
             const {nombre, descripcion, precio, existencia, fecha_elaboracion,fecha_vencimiento,tipo_producto,empresa,estado} = valores;
@@ -459,5 +459,5 @@ const NuevoProducto = () => {
         </Layout>
      );
 }
- 
+ {};
 export default NuevoProducto;
