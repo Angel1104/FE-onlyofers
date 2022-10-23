7b046b69671ad6a4a08d4eff22ac23427de8ff32
jhoel984 <201900813@est.umss.edu>
2022-10-23 10:48:08 +0400

    diseño en rejillas productos en home

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,32 @@ import * as Yup from 'yup';
 import {useQuery, useMutation,gql} from '@apollo/client';
 import Producto from '../componentes/cardProducto';
 
-const OBTENER_PRODUCTOS = gql`
-query ObtenerProductos {
-  obtenerProductos {
-    id
-    nombre_producto
-    descripcion_producto
-    precio
-    existencia
+
+
+export default function Index() {
+
+  //obtener productos de graphql
+  const{data,loading,error}=useQuery(OBTENER_PRODUCTOS);
+  if (loading) {
+    return 'cargando....'
   }
-}`
+
+  return (
+    <div>
+      <Layout>
+        <h1 className='text-2xl text-gray-800 font-light mb-4 '>Productos</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-x-6 gap-y-10">
+            {data.obtenerProductos.map(producto=>(
+              <Producto
+                Key={producto.id}
+                producto={producto}
+              />
+            ))}
+
+    </div>
+
+      </Layout>
+    </div>
+  )
+}
 
