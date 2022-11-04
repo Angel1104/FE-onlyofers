69c799d3f9bda0495e76fced0d66bb2d674f926b
RodriBau <rodri.bp.gg@gmail.com>
2022-11-04 19:52:41 -0400

    empresas casi completo

---
+++ componentes/tablaEmpresa.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {useQuery, useMutation,gql} from '@apollo/client';
+import Router from 'next/router';
+
+
+const ELIMINAR_EMPRESA = gql`
+mutation EliminarEmpresa($eliminarEmpresaId: ID!) {
+    eliminarEmpresa(id: $eliminarEmpresaId)
+  }
+`;
+const OBTENER_EMPRESAS = gql`
+query ObtenerEmpresas {
+  obtenerEmpresas {
+    nombre_empresa
+    numero_sucursal
+    direccion_empresa
+    telefono
+    tipo_empresa
+    id
+  }
+}`;
+
+const TablaEmpresa = ({empresa}) => {
+
+    //mutation para eliminar
+    const [eliminarEmpresa] = useMutation(ELIMINAR_EMPRESA, {
+        update(cache){
+            //obtener copia de cache
+            const {obtenerEmpresas} = cache.readQuery({query: OBTENER_EMPRESAS});
+            //reescribir cache
+            cache.writeQuery({
+                query: OBTENER_EMPRESAS,
+                data: {
+                    obtenerEmpresas: obtenerEmpresas.filter( empresaActual =>empresaActual.id !== id)
+                }
+            })
+        }
+    });
+
+    const{nombre_empresa,numero_sucursal,direccion_empresa,telefono,id} = empresa;
+
+    //eliminar empresa
+    const confirmarEliminarEmpresa= id =>{
+        Swal.fire({
+            title: 'Desea eliminar esta empresa?',
+            text: "Esta accion no se puede deshacer!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar',
+            cancelButtonText: 'No, Cancelar'
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+
+                try {
+                    //eliminar por id
+                    const {data} = await eliminarEmpresa({
+                        variables:{
+                            "eliminarEmpresaId": id
+                        }
+                    })
+                    //mostrar alerta
+                    Swal.fire(
+                        'Eliminado!',
+                        data.eliminarEmpresa,
+                        'success'
+                      )
+                } catch (error) {
+                    console.log(error)
+                }
+
+            }
+          })
+    };
+
+
+    const editarEmpresa =()=>{
+        Router.push({
+            pathname: "/editarempresa/[id]",
+            query:{id}
+        })
+    };
+    return ( 
+        <tr>
+            <td className='border px-4 py-2'>{nombre_empresa}</td>
+            <td className='border px-4 py-2'>{numero_sucursal}</td>
+            <td className='border px-4 py-2'>{direccion_empresa} </td>
+            <td className='border px-4 py-2'>{telefono}</td>
+            <td className='border px-4 py-2'>
+                <button 
+                    type="button" 
+                    className='flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xd uppercase font-bold '
+                    onClick={()=>confirmarEliminarEmpresa(id)}
+                    >
+                    
+                    Eliminar
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 ml-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+
+                </button>
+            </td>
+            <td className="border px-4 py-2"> 
+                    <button 
+                        type="button"
+                        className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                        onClick={()=> editarEmpresa()}
+                    >
+                        Editar
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                        <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+            </td>
+
+        </tr> 
+    )
+}
+ 
+export default TablaEmpresa;
---
+++ pages/nuevaempresa.js
@@ -0,0 +1,332 @@
+import React from 'react';
+import Layout from '../componentes/Layout';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import {useMutation,gql, useQuery} from '@apollo/client';
+import {useRouter} from 'next/router';
+import Swal from 'sweetalert2'
+import ComboTipEmpresas from '../componentes/ComboTipEmpresas';
+import Router from 'next/router';
+
+
+
+const NUEVA_EMPRESA=gql`
+mutation nuevoEmpresa($input: EmpresaInput){
+    nuevaEmpresa (input : $input){
+        id
+        nombre_empresa
+        numero_sucursal
+        direccion_empresa
+        telefono
+        tipo_empresa
+    }
+  }
+`;
+
+const OBTENER_EMPRESAS= gql`
+query ObtenerEmpresas {
+    obtenerEmpresas {
+      nombre_empresa
+      numero_sucursal
+      direccion_empresa
+      telefono
+    }
+  }
+`;
+
+const OBTENER_TIPO_EMPRESAS = gql`
+query ObtenerTiposEmpresas {
+    obtenerTiposEmpresas {
+      id
+      tipo_empresa
+    }
+  }
+`;
+
+const NuevoEmpresa = () => {
+    //routing
+    const router = useRouter();
+
+
+    //mutation para crear empresa
+    const  [nuevoEmpresa]= useMutation(NUEVO_EMPRESA
+        , {
+        update(cache, { data:{nuevoEmpresa}}){
+            // obtener el objeto de cache que deseamos actualizar
+            const { obtenerEmpresas} = cache.readQuery({ query: OBTENER_EMPRESAS});
+
+            // reeescriibr el cache( el cache nunca se debe modificar se reescribe)
+            cache.writeQuery({
+                query: OBTENER_EMPRESAS,
+                data:{
+                    obtenerEmpresas : [...obtenerEmpresas , nuevoEmpresa]
+                }
+            })
+        }
+    }
+    );
+
+    const confirmarAgregarEmpresa =()=>{
+        Swal.fire({
+            title: 'Desea Agregar esta Empresa?',
+            
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Agregar',
+            cancelButtonText: 'No, Cancelar'
+          })
+    };
+
+    const Cancelar =()=>{
+        Swal.fire({
+            title: 'Desea Cancelar el registro?',
+            text: "Volvera a pagina empresas ",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'si',
+            cancelButtonText: 'No'
+        }).then(async(result) => {
+            if (result.isConfirmed) {
+
+                Router.push({
+                    pathname: "/empresas"
+                    
+                })
+
+            }
+          })
+    };
+
+    
+//form para new empresa
+    const formik = useFormik({
+        initialValues:{
+            nombre : '',
+            sucursal: '',
+            direccion: '',
+            telefono:'',
+            tipo_empresa: ''
+        },
+        validationSchema: Yup.object({
+            nombre : Yup.string()
+                    .required('El Nombre es Obligatorio')
+                    .min(3, "El nombre tiene que tener al menos 3 carácteres")
+                    .max(50, "El nombre no puede superar los 50 carácteres")
+                    .matches(
+                        /^[aA-zZ\s]+$/,
+                        'No puede usar caracteres especiales'
+                      ),
+
+            sucursal : Yup.number()
+                      .required('El numero de Sucursal es Obligatorio')
+                      .positive('No se aceptan numeros negativos o "0"')
+                      .integer('El numero de Sucursal debe ser en numeros enteros'),
+
+            direccion: Yup.string()
+                      .required('La direccion es obligatoria')
+                      .min(3, "La direccion tiene que tener al menos 3 carácteres")
+                      .max(150, "La direccion no puede superar los 150 carácteres"),
+
+            telefono : Yup.number()
+                      .required('El  Telefono es Obligatorio')
+                      .positive('No se aceptan numeros negativos o "0"')
+                      .max(8)
+                      .min(7), 
+
+            tipo_empresa: Yup.string()
+                            .required('Se debe seleccionar el tipo de empresa')
+        }),
+        onSubmit: async valores => {
+            const {nombre, sucursal, direccion, telefono, tipo_empresa} = valores;
+            try {
+                const {data} = await nuevaEmpresa({
+                    variables:{
+                        input : {
+                            nombre_empresa: nombre,
+                            numero_sucursal: sucursal,
+                            direccion_empresa: direccion,
+                            telefono: telefono,
+                            tipo_empresa: tipo_empresa,
+                        } 
+                    }
+                });
+                //empresa creada correctamente mostrar mensaje
+                console.log(data)
+                Swal.fire(
+                    'Creado',
+                    'Creado correctamente',
+                    'success'
+                )
+                router.push('/empresas');
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    });
+    // obtener empresas de graphql
+    const empresas=useQuery(OBTENER_TIPO_EMPRESAS);
+    if (empresas.loading) {
+        return 'cargando...'
+    };
+    
+
+    return ( 
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-ligth text-center">Nuevo Empresa</h1>
+            <div className="flex justify-center mt-5 ">
+            <div className="w-full max-w-lg">
+                <form
+                    className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                    onSubmit={formik.handleSubmit}
+                >
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                    Nombre
+                                </label>
+                                <input
+                                    className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="nombre"
+                                    type="text"
+                                    placeholder="Nombre Empresa"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                            </div>
+                            {
+                                formik.touched.nombre && formik.errors.nombre ? (
+                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.nombre}</p>
+                                    </div>
+                                ) : null
+                            }
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="sucursal">
+                                    Número de Sucursal 
+                                </label>
+                                <input
+                                    className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="sucursal"
+                                    type="number"
+                                    placeholder="Número de Sucursal"
+                                    value={formik.values.sucursal}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                            </div>
+                            {
+                                formik.touched.sucursal && formik.errors.sucursal ? (
+                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.sucursal}</p>
+                                    </div>
+                                ) : null
+                            }
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
+                                    Teléfono
+                                </label>
+                                <input
+                                    className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="telefono"
+                                    type="number"
+                                    placeholder="Telefono"
+                                    value={formik.values.telefono}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                            </div>
+                            {
+                                formik.touched.telefono && formik.errors.telefono ? (
+                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.telefono}</p>
+                                    </div>
+                                ) : null
+                            }
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="direccion">
+                                    Direccion
+                                </label>
+                                <input
+                                    className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="direccion"
+                                    type="text"
+                                    placeholder="Dirección de la Empresa"
+                                    value={formik.values.direccion}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                            </div>
+                            {
+                                formik.touched.direccion && formik.errors.direccion ? (
+                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.direccion}</p>
+                                    </div>
+                                ) : null
+                            }
+
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tipo_empresa">
+                                    Tipo de Empresa
+                                </label>
+                                <select
+                                    className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="tipo_empresa"
+                                    value={formik.values.tipo_empresa}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                >
+                                    <option selected>selecione un tipo de empresa</option>
+                                    {empresas.data.obtenerTiposEmpresas.map(TipoEmpresa =>(
+                                        <ComboTipEmpresas
+                                            key={TipoEmpresa.id}
+                                            TipoEmpresa ={TipoEmpresa }
+                                        />
+                                    ))}
+                                </select>
+                            </div>
+                            {
+                                formik.touched.tipo_empresa && formik.errors.tipo_empresa ? (
+                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.tipo_empresa}</p>
+                                    </div>
+                                ) : null
+                            }
+
+                <button 
+                    type="submit" 
+                    className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+                    onClick={()=>confirmarAgregarProducto()}
+                    >
+                    
+                    AGREGAR NUEVA EMPRESA
+                    
+                </button>
+                
+                <button 
+                    type="submit" 
+                    className="bg-red-800 py-2 mt-2 px-4 w-full text-white uppercase hover:bg-gray-900"
+                    onClick={()=>Cancelar()}
+                    >
+                    Cancelar    
+                </button>
+                </form>
+            </div>
+        </div>
+        </Layout>
+     );
+}
+ {};
+export default NuevoEmpresa;
