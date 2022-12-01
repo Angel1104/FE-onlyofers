5cff6bb9eaf44f779432e23c26f93b2245b05757
veimar69494226 <jimenez.flores.veimar@gmail.com>
2022-12-01 22:26:20 +0100

    arreglo tipo producto

--- pages/nuevoproducto.js
+++ pages/nuevoproducto.js
@@ -189,6 +189,8 @@ const NuevoProducto = () => {
                     .required('La empresa es obligatoria'),
             tipo_producto: Yup.string()
                     .required('El tipo de producto es obligatorio')
+                    
+
         }),
         onSubmit: async valores => {
             const {nombre, descripcion, precio, existencia, fecha_elaboracion,fecha_vencimiento,tipo_producto,empresa,estado} = valores;
